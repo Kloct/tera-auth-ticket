bf294e610515dc2721b3bb83896fe2aa8c66b327
Kloct <Upfrontsuperior@gmail.com>
2020-03-18 01:09:53 -0400

    Updated README, Fixed ru Input bugs.

--- README.md
+++ README.md
@@ -7,10 +7,10 @@
 
 ```Javascript
 const webClient = require('tera-auth-ticket');
-const web = new webClient('(na, eu)', 'email', 'password');
+const web = new webClient('(na, eu, ru)', 'email', 'password', 'EUAccountName(optional)');
 
 web.getLogin((err, data) => {
-    if (err) console.log(err)
+    if (err) console.error(err)
     else console.log(data) // {name: accountName, ticket: ticket}
 })
 ```
@@ -21,15 +21,10 @@ Data can be used in `C_LOGIN_ARBITER` to login to the game servers.
 
 * No regions support IEsnare login currently
 * Other regions are planned to be added in the future
-* Support for Destiny logins is still in early stages and the official launcher will be required to complete any captchas or 2FA requests
+* `EUAccountName` is an optional parameter for Gameforge accounts with more than one tera account. You can specify which account you would like to use by setting this to the account's display name.
 
 ## Known Issues
 
-### RU
-
-* Logging in too many times from the same ip too quickly will add a captcha to the login process. As a temporary fix you need to fill out the captcha using the official launcher once and wait 10 minutes for it to dissapear.
-* Logging in from a new location for the first time will prompt you for 2FA. You will need to complete 2FA once on the official launcher to authorize your ip.
-
 ### EU
 
-* If you have multiple game accounts assiciated with a single Gameforge account you will only be able to access one of them.
+* If logging into multiple accounts, you can only login to up to 4 accounts ever 5 minutes or the account will be blocked.
--- regions/ru.js
+++ regions/ru.js
@@ -7,9 +7,6 @@ const axios = require('axios').default,
     fs = require('fs'),
     readline = require('readline');
 
-const rl = readline.createInterface({ input: process.stdin, output: process.stdout });
-function question(q) { return new Promise(resolve => { rl.question(q, resolve) }) };
-
 function makeHeadersDestiny(o) {
     return Object.assign({},
         {
@@ -39,8 +36,10 @@ function makeHeadersLauncher(o) {
 
 class webClient {
     constructor(email, password) {
-        this.email = email;
-        this.password = password;
+        this.creds = {
+            username: email,
+            password: password
+        }
         this.axiosDestiny = axios.create({
             baseURL: 'https://id.ddestiny.ru',
             headers: makeHeadersDestiny(),
@@ -60,104 +59,100 @@ class webClient {
     }
 
     async getLogin(){
-        try {
-            // Session Id
-            await this.axiosDestiny.get('/bar', { params: {project: 'tera'} });
-            // csrf
-            let popup = await this.axiosDestiny.get('/bar/popup/login/');
-            let creds = {
-                csrfmiddlewaretoken: popup.config.jar.toJSON().cookies.filter((cookie)=>cookie.key==='destinyid_csrftoken')[0].value,
-                username: this.email,
-                password: this.password
-            };
-            // captcha detection
-            if (popup.data.match(/\/captcha\/image\/.{41}/)) {
-                creds.captcha_0 = popup.data.match(/\/captcha\/image\/.{41}/)[0].match(/\w{40,40}/);
-                this.axiosDestiny.get(popup.data.match(/\/captcha\/image\/.{41}/)[0], { responseType: 'stream' })
-                    .then(res=> {
-                        res.data.pipe(fs.createWriteStream('captcha.jpg'))
-                    });
-                logThis.log('Captcha Detected!, Dowloaded to "node_modules/tera-auth-ticket/captcha.jpg".');
-                let captcha = await question('Enter the captcha solution: ');
-                creds.captcha_1 = captcha;
-                fs.unlink('captcha.jpg', err=>{ if (err) throw err });
-            };
-            // login
-            let login = await this.axiosDestiny({
+        // Session Id
+        await this.axiosDestiny.get('/bar', { params: {project: 'tera'} });
+        // csrf
+        let popup = await this.axiosDestiny.get('/bar/popup/login/');
+        this.creds.csrfmiddlewaretoken = popup.config.jar.toJSON().cookies.filter((cookie)=>cookie.key==='destinyid_csrftoken')[0].value
+        // login captcha detection
+        let captcha = await this.captchaCheck(popup)
+        if (captcha) Object.assign(this.creds, captcha)
+        // login
+        let login = await this.axiosDestiny({
+            method: 'post',
+            url: 'https://id.ddestiny.ru/bar/popup/login/', // need to specify full url for cookie to work
+            maxRedirects: 0,
+            headers: makeHeadersDestiny({
+                'Content-Type': 'application/x-www-form-urlencoded',
+                'Referer': 'https://id.ddestiny.ru/bar/popup/login/'
+            }),
+            data: querystring.stringify(this.creds)
+        });
+        // login OTP Check
+        await this.OTPCheck(login)
+        if (login.status!==302) throw `Failed to login! Incorrect username/password${this.creds.captcha_0?'/captcha':''}`;
+        logThis.log('Login Successful');
+        // Launcher OAuth
+        let oAuthStart = await this.axiosLauncher.get('/login/destinyid/?next=http%3A%2F%2Flauncher.tera-online.ru%2Flauncher%2F%3Fosid%3D4105790410', {maxRedirects: 0});
+        await this.axiosDestiny.get(oAuthStart.headers.location, {maxRedirects: 0});
+        await this.axiosDestiny.get('/oauth2/authorize/confirm', {maxRedirects: 0});
+        let oAuthRedirect = await this.axiosDestiny.get('/oauth2/redirect', {maxRedirects: 0});
+        let oAuthComplete = await this.axiosLauncher.get(oAuthRedirect.headers.location, {maxRedirects: 0});
+        logThis.log(`OAuth complete (Account: #${oAuthComplete.headers['x-accel-userid']})`);
+        // Auth ticket
+        let { data: authTicket } = await this.axiosLauncher.post('/launcher/get_otp/', {
+            headers: makeHeadersLauncher({
+                'X-Requested-With': 'XMLHttpRequest'
+            }),
+            data: 'source='
+        });
+        return {name: oAuthComplete.headers['x-accel-userid'], ticket: authTicket.otp};
+    };
+    async captchaCheck(response) {
+        if (response.data.match(/\/captcha\/image\/.{41}/)) {
+            const rl = readline.createInterface({ input: process.stdin, output: process.stdout });
+            function question(q) { return new Promise(resolve => { rl.question(q, resolve) }) };
+            this.axiosDestiny.get(response.data.match(/\/captcha\/image\/.{41}/)[0], { responseType: 'stream' })
+                .then(res=> {
+                    res.data.pipe(fs.createWriteStream('captcha.jpg')); // download captcha
+                });
+            logThis.log('Captcha Detected!, Dowloaded as "captcha.jpg".');
+            let captcha_1 = await question('Enter the captcha solution: ');
+            fs.unlink('captcha.jpg', err=>{ if (err) throw err });
+            rl.close()
+            return {captcha_0: response.data.match(/\/captcha\/image\/.{41}/)[0].match(/\w{40,40}/), captcha_1}
+        } else {
+            return false
+        }
+    }
+    async OTPCheck(response) {
+        if (response.headers.location === '/bar/popup/login/twofactor/?') {
+            const rl = readline.createInterface({ input: process.stdin, output: process.stdout });
+            function question(q) { return new Promise(resolve => { rl.question(q, resolve) }) };
+            let twoFactorRes = await this.axiosDestiny.get('https://id.ddestiny.ru/bar/popup/login/twofactor/?', {
+                headers: makeHeadersDestiny({
+                    'Referer': 'https://id.ddestiny.ru/bar/popup/login/'
+                })
+            });
+            logThis.log(`2FA Required to continue! An email with a One Time Password has been sent to ${this.email}`);
+            let code = await question('Enter OTP: ');
+            rl.close()
+            // Check Captcha in OTP Form
+            let twoFactorCaptcha = await this.captchaCheck(twoFactorRes)
+            // Post OTP Form
+            await this.axiosDestiny({
                 method: 'post',
-                url: 'https://id.ddestiny.ru/bar/popup/login/', // need to specify full url for cookie to work
+                url: 'https://id.ddestiny.ru/bar/popup/login/twofactor/?',
                 maxRedirects: 0,
                 headers: makeHeadersDestiny({
                     'Content-Type': 'application/x-www-form-urlencoded',
-                    'Referer': 'https://id.ddestiny.ru/bar/popup/login/'
+                    'Referer': 'https://id.ddestiny.ru/bar/popup/login/twofactor/?'
                 }),
-                data: querystring.stringify(creds)
-            });
-            if (login.headers.location === '/bar/popup/login/twofactor/?') {
-                let twoFactorRes = await this.axiosDestiny.get('https://id.ddestiny.ru/bar/popup/login/twofactor/?', {
-                    headers: makeHeadersDestiny({
-                        'Referer': 'https://id.ddestiny.ru/bar/popup/login/'
-                    })
+                data: querystring.stringify(
+                    Object.assign({
+                        'otp_challenge': '',
+                        'csrfmiddlewaretoken': this.creds.csrfmiddlewaretoken,
+                        'otp_token': code
+                    }, twoFactorCaptcha)
+                )
+            })
+                .then(res=>{
+                    if (res.status!=302) throw `Incorrect OTP${twoFactorCaptcha.captcha_1?' or Captcha':''}!`;
                 });
-                logThis.log(`2FA Required to continue! An email with a One Time Password has been sent to ${this.email}`);
-                let code = await question('Enter OTP: ');
-                // Check Captcha in OTP Form
-                let twoFactorCaptcha = {};
-                if (twoFactorRes.data.match(/\/captcha\/image\/.{41}/)) {
-                    twoFactorCaptcha.captcha_0 = twoFactorRes.data.match(/\/captcha\/image\/.{41}/)[0].match(/\w{40,40}/);
-                    this.axiosDestiny.get(twoFactorRes.data.match(/\/captcha\/image\/.{41}/)[0], { responseType: 'stream' })
-                        .then(res=> {
-                            res.data.pipe(fs.createWriteStream('captcha.jpg'));
-                        });
-                    logThis.log('Captcha Detected in OTP!, Dowloaded to "node_modules/tera-auth-ticket/captcha.jpg".');
-                    let captcha = await question('Enter the captcha solution: ');
-                    twoFactorCaptcha.captcha_1 = captcha;
-                    fs.unlink('captcha.jpg', err=>{ if (err) throw err });
-                };
-                // Post OTP Form
-                await this.axiosDestiny({
-                    method: 'post',
-                    url: 'https://id.ddestiny.ru/bar/popup/login/twofactor/?',
-                    maxRedirects: 0,
-                    headers: makeHeadersDestiny({
-                        'Content-Type': 'application/x-www-form-urlencoded',
-                        'Referer': 'https://id.ddestiny.ru/bar/popup/login/twofactor/?'
-                    }),
-                    data: querystring.stringify(
-                        Object.assign({
-                            'otp_challenge': '',
-                            'csrfmiddlewaretoken': creds.csrfmiddlewaretoken,
-                            'otp_token': code
-                        }, twoFactorCaptcha)
-                    )
-                })
-                    .then(res=>{
-                        if (res.status!=302) throw `Incorrect OTP${twoFactorCaptcha.captcha_1?' or Captcha':''}!`;
-                    });
-            };
-            if (login.status!==302) throw `Failed to login! Incorrect username/password${creds.captcha_0?'/captcha':''}`;
-            logThis.log('Login Successful');
-            // Launcher OAuth
-            let oAuthStart = await this.axiosLauncher.get('/login/destinyid/?next=http%3A%2F%2Flauncher.tera-online.ru%2Flauncher%2F%3Fosid%3D4105790410', {maxRedirects: 0});
-            await this.axiosDestiny.get(oAuthStart.headers.location, {maxRedirects: 0});
-            await this.axiosDestiny.get('/oauth2/authorize/confirm', {maxRedirects: 0});
-            let oAuthRedirect = await this.axiosDestiny.get('/oauth2/redirect', {maxRedirects: 0});
-            let oAuthComplete = await this.axiosLauncher.get(oAuthRedirect.headers.location, {maxRedirects: 0});
-            logThis.log(`OAuth complete (Account: #${oAuthComplete.headers['x-accel-userid']})`);
-            // Auth ticket
-            let { data: authTicket } = await this.axiosLauncher.post('/launcher/get_otp/', {
-                headers: makeHeadersLauncher({
-                    'X-Requested-With': 'XMLHttpRequest'
-                }),
-                data: 'source='
-            });
-            rl.close();
-            return {name: oAuthComplete.headers['x-accel-userid'], ticket: authTicket.otp};
-        } catch(err){
-            rl.close();
-            throw err;
-        };
-        
-    };
+            return
+        } else {
+            return false
+        }
+    }
 };
 module.exports = webClient;
